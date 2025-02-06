b30a3fa9c3d07e6c252e6734e1543b5085e7fc8b
cch137 <96408836+cch137@users.noreply.github.com>
2025-02-06 17:55:59 +0800

    feat: sleep abort signal

--- src/sleep/index.ts
+++ src/sleep/index.ts
@@ -1,3 +1,22 @@
-export default async function sleep(ms: number) {
-  return new Promise((r) => setTimeout(r, ms));
+export type SleepOptions = { signal?: AbortSignal };
+
+export default async function sleep(
+  ms: number,
+  options?: SleepOptions
+): Promise<void>;
+export default async function sleep(ms: number, { signal }: SleepOptions = {}) {
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(resolve, ms);
+
+    if (signal) {
+      signal.addEventListener(
+        "abort",
+        (e) => {
+          clearTimeout(timeout);
+          reject(signal.reason);
+        },
+        { once: true }
+      );
+    }
+  });
 }
