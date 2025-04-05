6908b889da9e5418034a5424cb50a34a73c3d9d3
cch137 <96408836+cch137@users.noreply.github.com>
2025-04-05 18:16:14 +0800

    add asyncDelay

--- src/sleep/index.ts
+++ src/sleep/index.ts
@@ -20,3 +20,20 @@ export default async function sleep(ms: number, { signal }: SleepOptions = {}) {
     }
   });
 }
+
+export function asyncDelay<T extends (...args: any[]) => Promise<any>>(
+  func: T,
+  durationMs: number
+): T {
+  if (!durationMs) return func;
+  return (async (...args: Parameters<T>) => {
+    const timer = sleep(durationMs, {});
+    try {
+      const result = await func(...args);
+      await timer;
+      return result;
+    } catch (e) {
+      throw e;
+    }
+  }) as T;
+}
